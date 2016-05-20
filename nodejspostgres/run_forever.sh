5adbde41be12c9d062440bedd92361b927d8f4b1
thanh <thanh.nguyen2891@gmail.com>
2016-05-20 21:54:09 +0700

    test

--- nodejspostgres/run_forever.sh
+++ nodejspostgres/run_forever.sh
@@ -3,3 +3,4 @@ log_file=logs/log_`date +%Y%m%d-%H%M`
 forever  -o $log_file  start --spinSleepTime 10000 run.js config_production.json >> logs/forever.txt 2>&1
 cat logs/forever.txt
 rm logs/forever.txt
+////////////////////////////
